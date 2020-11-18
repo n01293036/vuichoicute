64c74453f3a28fcdedca379930fcc4a279ef6a05
n01293036@humbermail.ca <Canhduc1>
2020-11-17 20:04:54 -0500

    first commit

---
+++ app/src/main/java/assignment1/example/vuichoicute/lightcontrol1.java
@@ -0,0 +1,14 @@
+package assignment1.example.vuichoicute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lightcontrol1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lightcontrol1);
+    }
+}
